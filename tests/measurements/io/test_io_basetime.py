b139933cc48fd045cc69dd91ffcb1fafc542a15a
Robin Wimmers <r.wimmers@cemsbv.io>
2024-07-22 17:32:12 +0200

    test(basetime): refactor codebase test

---
+++ tests/measurements/io/test_io_basetime.py
@@ -0,0 +1,41 @@
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from baec.measurements.io.basetime import BaseTimeBucket, Credentials
+
+
+def test_io_basetime() -> None:
+    """
+    Test script for the Basetime connection.
+    Credentials file is not loaded in env, but stored locally
+
+    testing:
+        -Output when calling company, projects, object IDs
+        -Output when calling a series of an object ID
+    """
+    show = False
+
+    # get AWS credentials
+    filepath = os.path.join(os.path.dirname(__file__), "cems_accessKeys.csv")
+    if os.path.isfile(filepath):
+        credentials = Credentials.from_csv(filepath)
+    else:
+        credentials = Credentials()
+
+    manage_project = BaseTimeBucket(credentials)
+
+    print(manage_project.get_users_projects_ids())
+    series = manage_project.make_settlement_rod_measurement_series(
+        company="Demo", project="Hansweert", rod_id="277-2"
+    )
+    assert isinstance(series.to_dataframe(), pd.DataFrame)
+
+    series.plot_xyz_time()
+
+    # Show the plots
+    if show:
+        plt.show()
+
+    plt.close("all")
