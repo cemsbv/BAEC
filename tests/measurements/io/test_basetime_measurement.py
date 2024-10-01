1d1e6060ab8f1614031d2daaa3923e05ea5b3f4b
Tim <tim.bogers@basetime.nl>
2024-10-01 12:10:01 +0200

    Update Lambda and super linter, restful 01 version

--- tests/measurements/io/test_basetime_measurement.py
+++ tests/measurements/io/test_basetime_measurement.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 from baec.measurements.basetime_measurements import ProjectsIDs
 
@@ -10,12 +11,23 @@ testing:
     -Output when calling company, projects, object IDs
     -Output when calling a series of an object ID
 """
-
+print(datetime.now())
+time_start = datetime.now()
 with open("cems_accessKeys.csv") as credfile:
     manage_project = ProjectsIDs(credfile)
-
+print(datetime.now()-time_start)
+time_start = datetime.now()
 print(manage_project.get_users_projects_ids())
 test_series = manage_project.make_SettlementRodMeasurementSeries(
-    company="Van Oord", project="Voorbelasting Wilderszijde Lansingerland", rod_id="ZB1001"
+    project="Voorbelasting Wilderszijde Lansingerland", rod_id="ZB-1001"
+)
+
+print(test_series.to_dataframe())
+print(datetime.now()-time_start)
+time_start = datetime.now()
+test_series = manage_project.make_SettlementRodMeasurementSeries(
+    project="Voorbelasting Wilderszijde Lansingerland", rod_id="ZB-1001"
 )
 print(test_series.to_dataframe())
+print(datetime.now()-time_start)
+time_start = datetime.now()
